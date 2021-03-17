df17ce8112134d0dc3be0000fd43a184d1b7e21e
Ubuntu <ubuntu@ip-172-31-41-204.eu-central-1.compute.internal>
2021-03-17 12:13:15 +0000

    Several more runs for LSTM with retrain.

---
+++ global_run/nn_second_call.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import multiprocessing
+
+import lstm_retrain
+import lstm_discard
+import condrnn
+
+if __name__ == "__main__":
+	processes = []
+	list_of_names = ["linear1_abrupt", "linear2_abrupt", "linear3_abrupt", "nonlinear1_abrupt", "nonlinear2_abrupt", "nonlinear3_abrupt"]
+	for iteration in range(1, 3):
+		for name in list_of_names:
+			p = multiprocessing.Process(target = lstm_retrain.main, args = (iteration, name, ))
+			processes.append(p)
+			p.start()
+
+	for process in processes:
+		process.join()
